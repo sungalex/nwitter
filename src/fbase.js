813625d0c70b30c020690f7b1dd2f6a53ca98260
sungalex <cloudallera@gmail.com>
2020-12-23 03:47:41 +0900

    router setup

--- src/fbase.js
+++ src/fbase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
-    appId: process.env.REACT_APP_APP_ID
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
