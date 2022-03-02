eb50a2bf39279ee27ae7f1f47c9d586ff5810f9e
sungalex <cloudallera@gmail.com>
2022-03-02 23:27:27 +0900

    firestore setup and Nweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,17 @@
+import { dbService } from "fbService";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import React, { useState } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    const docRef = await addDoc(collection(dbService, "nweets"), {
+      nweet,
+      createdAt: serverTimestamp(),
+    });
+    console.log("Document written with ID: ", docRef.id);
+    setNweet("");
   };
   const onChange = (event) => {
     const {
@@ -19,7 +27,7 @@ const Home = () => {
           onChange={onChange}
           type="text"
           placeholder="What's on your mind?"
-          maxLehgth={120}
+          maxLength={120}
         />
         <input type="submit" value="Nweet" />
       </form>
