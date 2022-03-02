832c53e3ed381776660a5991e818fde8824e77ef
sungalex <cloudallera@gmail.com>
2022-03-03 00:54:01 +0900

    Getting the Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,24 +1,48 @@
 import { dbService } from "fbService";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import React, { useState } from "react";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  getDocs,
+} from "firebase/firestore";
+import React, { useCallback, useEffect, useState } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
   const onSubmit = async (event) => {
     event.preventDefault();
     const docRef = await addDoc(collection(dbService, "nweets"), {
-      nweet,
+      msg: nweet,
       createdAt: serverTimestamp(),
     });
     console.log("Document written with ID: ", docRef.id);
     setNweet("");
   };
+
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
     setNweet(value);
   };
+
+  const getNweets = useCallback(async () => {
+    const dbNweets = await getDocs(collection(dbService, "nweets"));
+    dbNweets.forEach((doc) => {
+      const nweetObj = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setNweets((prev) => [nweetObj, ...prev]);
+    });
+  }, []);
+
+  useEffect(() => {
+    getNweets();
+  }, [getNweets]);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -31,6 +55,13 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.msg}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
