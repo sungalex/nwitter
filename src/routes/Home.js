59875ae2a5fff4dfa0602da78236ba6ff2e1313c
sungalex <cloudallera@gmail.com>
2022-03-02 22:30:37 +0900

    adding form

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLehgth={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
