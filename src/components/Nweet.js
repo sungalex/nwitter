abdd6f8b6099705f7c21ea90a4abfcd38438ed6a
sungalex <cloudallera@gmail.com>
2022-03-04 06:12:13 +0900

    show nweet image

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -49,6 +49,14 @@ const Nweet = ({ nweetObj, isOwner }) => {
       ) : (
         <>
           <h4>{nweetObj.text}</h4>
+          {nweetObj.attachmentUrl && (
+            <img
+              src={nweetObj.attachmentUrl}
+              width="50px"
+              height="50px"
+              alt=""
+            />
+          )}
           {isOwner && (
             <>
               <button onClick={onDeleteClick}>Delete Nweet</button>
