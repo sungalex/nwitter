b8e2374c6b7fd34bdc7ace0b2d5e77fe29fc7a3d
sungalex <cloudallera@gmail.com>
2022-03-04 02:51:18 +0900

    delete and update part two

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,15 +1,64 @@
-import React from "react";
+import { dbService } from "fbService";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
 
-const Nweet = ({ nweetObj, isOwner }) => (
-  <div>
-    <h4>{nweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete Nweet</button>
-        <button>Edit Nweet</button>
-      </>
-    )}
-  </div>
-);
+const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this nweet?");
+    if (ok) {
+      await deleteDoc(doc(dbService, "nweets", `${nweetObj.id}`));
+    }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewNweet(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await updateDoc(doc(dbService, "nweets", `${nweetObj.id}`), {
+      text: newNweet,
+    });
+    setEditing(false);
+  };
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your nweet"
+              value={newNweet}
+              onChange={onChange}
+              required
+            />
+            <input type="submit" onClick={onSubmit} value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Nweet;
