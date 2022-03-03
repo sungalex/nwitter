de2b4e2e4b31bdf713da8e14a468c2da42c565dc
sungalex <cloudallera@gmail.com>
2022-03-04 05:33:10 +0900

    Uploading image

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,14 @@
 import Nweet from "components/Nweet";
-import { dbService } from "fbService";
+import { dbService, storageService } from "fbService";
 import {
   addDoc,
   collection,
   serverTimestamp,
   onSnapshot,
 } from "firebase/firestore";
+import { ref, uploadString } from "firebase/storage";
 import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
@@ -15,13 +17,24 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
+    let imageFullPath;
+    if (attachment) {
+      const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+      const response = await uploadString(fileRef, attachment, "data_url");
+      console.log(response);
+      imageFullPath = response.ref.fullPath;
+    } else {
+      imageFullPath = "";
+    }
     await addDoc(collection(dbService, "nweets"), {
       text: nweet,
       createdAt: serverTimestamp(),
       creatorId: userObj.uid,
       creatorName: userObj.displayName,
+      imageUrl: imageFullPath,
     });
     setNweet("");
+    setAttachment(null);
   };
 
   const onChange = (event) => {
@@ -40,7 +53,7 @@ const Home = ({ userObj }) => {
       }));
       setNweets(nweetArray);
     });
-  }, []);
+  }, [attachment]);
 
   const onFileChange = (event) => {
     const {
@@ -69,8 +82,9 @@ const Home = ({ userObj }) => {
           type="text"
           placeholder="What's on your mind?"
           maxLength={120}
+          required
         />
-        <input type="file" accetp="image/*" onChange={onFileChange} />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Nweet" />
         {attachment && (
           <div>
