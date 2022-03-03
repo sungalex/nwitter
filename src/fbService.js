b5f2ea55c5a7d9bb75f61d91d13f560c399ec7b2
sungalex <cloudallera@gmail.com>
2022-03-04 05:32:30 +0900

    add firebase storage service

--- src/fbService.js
+++ src/fbService.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const authService = getAuth(app);
 export const dbService = getFirestore(app);
+export const storageService = getStorage(app);
