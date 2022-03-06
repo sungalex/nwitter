0157167a129e95abd30b95da265e29b6823b74de
sungalex <cloudallera@gmail.com>
2022-03-06 22:12:54 +0900

    apply CSS with FontAwsomeIcon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,3 +1,6 @@
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -6,12 +9,34 @@ const Navigation = ({ userName, userObj }) => {
 
   return (
     <nav>
-      <ul>
+      <ul
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 50,
+        }}>
         <li>
-          <Link to="/">Home</Link>
+          <Link
+            to="/"
+            style={{ display: "flex", alignItems: "center", marginRight: 10 }}>
+            <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="2x" />
+          </Link>
         </li>
         <li>
-          <Link to="/profile">{userName}'s Profile</Link>
+          <Link
+            to="/profile"
+            style={{
+              marginLeft: 10,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              fontSize: 12,
+            }}>
+            <FontAwesomeIcon icon={faUser} color={"#04AAFF"} size="2x" />
+            <span style={{ marginTop: 10 }}>
+              {userName ? `${userName}'s Profile` : "Profile"}
+            </span>
+          </Link>
         </li>
       </ul>
     </nav>
